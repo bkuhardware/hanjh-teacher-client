782d1022eb3e20c2399a6dbf63962fd186d80c53
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-02-15 15:51:49 +0700

    fix ui bug

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ export default {
     namespace: 'user',
     state: {
         name: 'Ngoc Hanh Vuong',
-        avatar: null,
+        avatar: "https://scontent.fdad1-1.fna.fbcdn.net/v/t1.0-9/32693290_1729250587169728_2501159919764373504_o.jpg?_nc_cat=104&_nc_oc=AQmLZedMfuo9uONVez_pontQ0TqMhBv3Z-Mp-6Hq6GgjFReu5-W0sSfdgN5dxtz2IHk&_nc_ht=scontent.fdad1-1.fna&oh=c8604c3b1dbfac1191570dc7774bc025&oe=5EC92867",
         email: 'ngochanhvuong@gmail.com',
         noOfUsNotification: 22,
     },
