ee9d6df3698236d6f9bca10a6972f480f8a56915
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-02-15 14:38:51 +0700

    basic layout init

--- src/layouts/BasicLayout/index.js
+++ src/layouts/BasicLayout/index.js
@@ -1,8 +1,28 @@
-export default ({ children }) => {
+import React from 'react';
+import { Layout } from 'antd';
+import Header from '@/components/Header';
+import Sider from '@/components/Sider';
+import Footer from '@/components/Footer';
+import ScrollLayout from '@/components/ScrollLayout';
+import styles from './index.less';
+
+const { Content } = Layout;
+
+const BasicLayout = ({ children }) => {
     return (
-        <div>
-            <div>Basic layout</div>
-            <div>{children}</div>
-        </div>
+        <Layout className={styles.basicLayout}>
+            <Header />
+            <ScrollLayout>
+                <Sider />
+                <Layout>
+                    <Content>
+                        {children}
+                    </Content>
+                    <Footer />
+                </Layout>
+            </ScrollLayout>
+        </Layout>
     )
 };
+
+export default BasicLayout;
