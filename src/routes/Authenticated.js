c10fae145c6b7702ad940d809ee3c0d3f0993b72
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-02-17 12:33:13 +0700

    authorized

--- src/routes/Authenticated.js
+++ src/routes/Authenticated.js
@@ -1,3 +1,44 @@
-export default ({ children }) => {
-    return children;
-}
+import React, { useState, useEffect } from 'react';
+import { connect } from 'dva';
+import withRouter from 'umi/withRouter';
+import Redirect from 'umi/redirect';
+import PageLoading from '@/components/PageLoading';
+import FinishInfo from '@/components/FinishInfo';
+import storage from '@/utils/storage';
+
+const Authenticated = ({ user, children, location, dispatch }) => {
+    const [status, setStatus] = useState('pending');
+    useEffect(() => {
+        if (user) setStatus('authenticated');
+        else {
+            const token = storage.getToken();
+            if (token)
+                dispatch({
+                    type: 'user/fetch',
+                    payload: {
+                        callback: () => setStatus('authenticated')
+                    }
+                })
+            else setStatus('not-authenticated');
+        }
+    }, []);
+    const check = user => {
+        return false;
+    };
+    if (status === 'pending') return <PageLoading />;
+    if (status === 'not-authenticated') 
+        return (
+            <Redirect
+                to={{
+                    pathname: '/user/login',
+                    state: {
+                        from: location.pathname
+                    }
+                }}
+            />
+        );
+    if (check(user)) return children;
+    return <FinishInfo />;
+};
+
+export default withRouter(connect(({ user }) => ({ user }))(Authenticated));
