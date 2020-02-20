073c3628ecf63cc4fff40e41871d3a4bacd87749
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-02-20 11:53:03 +0700

    settings part 2 -> basic info

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,21 +1,52 @@
 import React, { useState } from 'react';
 import { connect } from 'dva';
 import { Tabs, Form, Input, Upload, Avatar, Icon, Button } from 'antd';
-import { EditorState } from 'draft-js';
+import { EditorState, ContentState, convertFromHTML } from 'draft-js';
 import Editor from '@/components/Editor/SimpleEditor';
 import Wrapper from '@/components/PageWrapper';
 import { capitalText } from '@/utils/utils';
 import styles from './index.less';
 
 const { TabPane } = Tabs;
+const FormItem = Form.Item;
 
-const Settings = ({ dispatch, ...props }) => {
+const Settings = ({ dispatch, form, ...props }) => {
     const {
         user
     } = props;
+    const { getFieldDecorator } = form;
     const [avatar, setAvatar] = useState(null);
     const [avatarLoading, setAvatarLoading] = useState(false);
     const [fileList, setFileList] = useState([]);
+    const blocksFromHTML = convertFromHTML(user.biography);
+    const bio = ContentState.createFromBlockArray(
+        blocksFromHTML.contentBlocks,
+        blocksFromHTML.entityMap,
+    );
+
+    const [biography, setBiography] = useState({
+        value: EditorState.createWithContent(bio),
+        validateStatus: 'success',
+        help: ''
+    });
+
+    const handleChangeBiography = newBiography => {
+        const newContent = newBiography.getCurrentContent();
+        if (newContent.hasText()) {
+            setBiography({
+                value: newBiography,
+                validateStatus: 'success',
+                help: ''
+            });
+        }
+        else {
+            setBiography({
+                value: newBiography,
+                validateStatus: 'error',
+                help: 'You must enter biography!'
+            });
+        }
+    };
     const handleBeforeUpload = (file, fileList) => {
         setAvatar(file);
         setFileList(fileList);
@@ -92,18 +123,86 @@ const Settings = ({ dispatch, ...props }) => {
                         </div>
                     </div>
                 </div>
-                <div className={styles.tabs}>
-                    <Tabs defaultActiveKey="profile">
-                        <TabPane key="profile" tab="Basic information">
+                <div className={styles.fixed}></div>
+                <div className={styles.info}>
+                    <Tabs
+                        defaultActiveKey="profile"
+                        className={styles.tabs}
+                        tabBarStyle={{
+                            borderBottom: 'none',
+                            textAlign: 'center'
+                        }}
+                    >
+                        <TabPane className={styles.tabPane} key="profile" tab="Basic information">
+                            <Form className={styles.basicInfo}>
+                                <FormItem label="Name" required>
+                                    {getFieldDecorator('name', {
+                                        rules: [
+                                            {
+                                                required: true,
+                                                message: 'You must enter your name!'
+                                            }
+                                        ],
+                                        initialValue: user.name
+                                    })(<Input placeholder="Name" size="large"/>)}
+                                </FormItem>
+                                <FormItem label="Email" required>
+                                    {getFieldDecorator('email', {
+                                        rules: [
+                                            {
+                                                required: true,
+                                                message: 'You must enter your email!'
+                                            },
+                                            {
+                                                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                                message: 'Your email is invalid!'
+                                            }
+                                        ],
+                                        initialValue: user.email
+                                    })(<Input placeholder="email" size="large" />)}
+                                </FormItem>
+                                <FormItem label="Headline" required>
+                                    {getFieldDecorator('job', {
+                                        rules: [
+                                            {
+                                                required: true,
+                                                message: 'You must enter your headline!'
+                                            },
+                                            {
+                                                min: 60,
+                                                message: 'Your headline must has at least 60 characters!'
+                                            }
+                                        ],
+                                        initialValue: user.job
+                                    })(<Input placeholder="Headline (Job)" size="large" addonAfter={form.getFieldValue('job').length < 60 ? `(${60 - form.getFieldValue('job').length})` : <Icon type="check" />}/>)}
+                                </FormItem>
+                                <FormItem
+                                    label="Biography"
+                                    className={styles.bio}
+                                    help={biography.help}
+                                    validateStatus={biography.validateStatus}
+                                    required>
+                                    <Editor
+                                        editorState={biography.value}
+                                        onChange={handleChangeBiography}
+                                        minCount={100}
+                                        placeholder="Biography"
+                                    />
+                                </FormItem>
+                                <FormItem style={{ textAlign: 'center' }}>
+                                    <Button className={styles.btn} htmlType="button" type="primary" onClick={() => {}} size="large">
+                                        Update information
+                                    </Button>
+                                </FormItem>
+                            </Form>
+                        </TabPane>
+                        <TabPane className={styles.tabPane} key="social" tab="Social links">
                             
                         </TabPane>
-                        <TabPane key="social" tab="Social links">
+                        <TabPane className={styles.tabPane} key="password" tab="Change password">
                             
                         </TabPane>
-                        <TabPane key="password" tab="Change password">
-                            
-                        </TabPane>
-                        <TabPane key="payment" tab="Payments">
+                        <TabPane className={styles.tabPane} key="payment" tab="Payments">
                             
                         </TabPane>
                     </Tabs>
@@ -112,8 +211,8 @@ const Settings = ({ dispatch, ...props }) => {
     )
 };
 
-export default connect(
+export default Form.create()(connect(
     ({ user, loading }) => ({
         user: user
     })
-)(Settings);
+)(Settings));
