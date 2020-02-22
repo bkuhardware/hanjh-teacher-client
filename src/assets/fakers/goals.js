c377c87c38a5bd0c1dee2a105955e2132bfbce43
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-02-22 10:36:39 +0700

    goals ui part 2

--- src/assets/fakers/goals.js
+++ src/assets/fakers/goals.js
@@ -1,16 +1,124 @@
 export default {
     whatLearn: [
-        'Get in-depth knowledge of <strong>Vue JS</strong> & <strong>Firebase</strong> from the ground up',
-        'Build & deploy 3 real-world web apps with Vue JS & Firebase',
-        'Learn about & implement Firebase authentication into Vue JS web apps',
-        'Use other Firebase services such as a Firestore database, Cloud Functions & Hosting',
-        'Understand how using frameworks like Django will save you a ton of time in web development'
+        {
+            _id: 1,
+            content: 'Get in-depth knowledge of <strong>Vue JS</strong> & <strong>Firebase</strong> from the ground up',
+            owner: {
+                //last owner
+                _id: 1,
+                name: 'Thuy Huyen',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/83644753_1765496750259122_7261950568300544000_n.jpg?_nc_cat=105&_nc_oc=AQl7P3ybDMEUjJF6QEXVNp8UrrWIa57YPrTPfWqVDNFlBf7cCmOyX7Re115oUGS88EA&_nc_ht=scontent.fsgn5-2.fna&oh=cb09c64994cffa8ab83c09f0e199440a&oe=5EC33792'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: false
+        },
+        {
+            _id: 2,
+            content: 'Build & deploy 3 real-world web apps with Vue JS & Firebase',
+            owner: {
+                //last owner
+                _id: 2,
+                name: 'Trong Luan',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-9/83558548_2290286491264377_331290296627232768_n.jpg?_nc_cat=107&_nc_oc=AQnpLi8nWGbC-08nBlYjhCuZyGVkcZMFHaqWTcBFbEZK1GzrkY73FWhSwonUwq-m0aE&_nc_ht=scontent.fsgn5-2.fna&oh=84246739e4ba3279ce49566f8f59bb01&oe=5EC49D12'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: true
+        },
+        {
+            _id: 3,
+            content: 'Learn about & implement Firebase authentication into Vue JS web apps',
+            owner: {
+                //last owner
+                _id: 1,
+                name: 'Thuy Huyen',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/83644753_1765496750259122_7261950568300544000_n.jpg?_nc_cat=105&_nc_oc=AQl7P3ybDMEUjJF6QEXVNp8UrrWIa57YPrTPfWqVDNFlBf7cCmOyX7Re115oUGS88EA&_nc_ht=scontent.fsgn5-2.fna&oh=cb09c64994cffa8ab83c09f0e199440a&oe=5EC33792'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: false
+        },
+        {
+            _id: 4,
+            content: 'Use other Firebase services such as a Firestore database, Cloud Functions & Hosting',
+            owner: {
+                //last owner
+                _id: 1,
+                name: 'Thuy Huyen',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/83644753_1765496750259122_7261950568300544000_n.jpg?_nc_cat=105&_nc_oc=AQl7P3ybDMEUjJF6QEXVNp8UrrWIa57YPrTPfWqVDNFlBf7cCmOyX7Re115oUGS88EA&_nc_ht=scontent.fsgn5-2.fna&oh=cb09c64994cffa8ab83c09f0e199440a&oe=5EC33792'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: false
+        },
+        {
+            _id: 5,
+            content: 'Understand how using frameworks like Django will save you a ton of time in web development',
+            owner: {
+                //last owner
+                _id: 2,
+                name: 'Trong Luan',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-9/83558548_2290286491264377_331290296627232768_n.jpg?_nc_cat=107&_nc_oc=AQnpLi8nWGbC-08nBlYjhCuZyGVkcZMFHaqWTcBFbEZK1GzrkY73FWhSwonUwq-m0aE&_nc_ht=scontent.fsgn5-2.fna&oh=84246739e4ba3279ce49566f8f59bb01&oe=5EC49D12'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: true
+        }
     ],
     requirements: [
-        'You will need a computer and an interest in learning Django!',
-        'Some experience with coding in any language is a plus, but not required',
-        'ES6 Knowledge is advantageous',
-        'An appreciation of AJAX is a plus but not essential'
+        {
+            _id: 1,
+            content: 'You will need a computer and an interest in learning Django!',
+            owner: {
+                //last owner
+                _id: 1,
+                name: 'Thuy Huyen',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/83644753_1765496750259122_7261950568300544000_n.jpg?_nc_cat=105&_nc_oc=AQl7P3ybDMEUjJF6QEXVNp8UrrWIa57YPrTPfWqVDNFlBf7cCmOyX7Re115oUGS88EA&_nc_ht=scontent.fsgn5-2.fna&oh=cb09c64994cffa8ab83c09f0e199440a&oe=5EC33792'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: false
+        },
+        {
+            _id: 2,
+            content: 'Some experience with coding in any language is a plus, but not required',
+            owner: {
+                //last owner
+                _id: 1,
+                name: 'Thuy Huyen',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/83644753_1765496750259122_7261950568300544000_n.jpg?_nc_cat=105&_nc_oc=AQl7P3ybDMEUjJF6QEXVNp8UrrWIa57YPrTPfWqVDNFlBf7cCmOyX7Re115oUGS88EA&_nc_ht=scontent.fsgn5-2.fna&oh=cb09c64994cffa8ab83c09f0e199440a&oe=5EC33792'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: false
+        },
+        {
+            _id: 3,
+            content: 'ES6 Knowledge is advantageous',
+            owner: {
+                //last owner
+                _id: 1,
+                name: 'Thuy Huyen',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-1/83644753_1765496750259122_7261950568300544000_n.jpg?_nc_cat=105&_nc_oc=AQl7P3ybDMEUjJF6QEXVNp8UrrWIa57YPrTPfWqVDNFlBf7cCmOyX7Re115oUGS88EA&_nc_ht=scontent.fsgn5-2.fna&oh=cb09c64994cffa8ab83c09f0e199440a&oe=5EC33792'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: false
+        },
+        {
+            _id: 4,
+            content: 'An appreciation of AJAX is a plus but not essential',
+            owner: {
+                //last owner
+                _id: 2,
+                name: 'Trong Luan',
+                avatar: 'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-9/83558548_2290286491264377_331290296627232768_n.jpg?_nc_cat=107&_nc_oc=AQnpLi8nWGbC-08nBlYjhCuZyGVkcZMFHaqWTcBFbEZK1GzrkY73FWhSwonUwq-m0aE&_nc_ht=scontent.fsgn5-2.fna&oh=84246739e4ba3279ce49566f8f59bb01&oe=5EC49D12'
+            },
+            createdAt: 1567476499584,
+            updatedAt: 1567476499584,
+            editable: true
+        },
     ],
     targetStudents: [
         
--- src/pages/Course/Edit/Goals.js
+++ src/pages/Course/Edit/Goals.js
@@ -1,26 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
+import moment from 'moment';
 import { connect } from 'dva';
-import { Alert, Input, Icon, Collapse, Badge, Skeleton, Spin } from 'antd';
+import { Alert, Input, Icon, Collapse, Badge, Skeleton, Spin, Avatar, Tooltip, Button, Row, Col, Modal } from 'antd';
 import styles from './Goals.less';
 
 const { Panel } = Collapse;
+const ButtonGroup = Button.Group;
+
+const TargetItem = ({ item, onChange }) => {
+    const [content, setContent] = useState(item.content);
+    const [owner, setOnwer] = useState({ ...item.owner });
+    const [updatedAt, setUpdatedAt] = useState(item.updatedAt);
+    const [type, setType] = useState('text');
+    const [extraVisible, setExtraVisible] = useState(false);
+    if (type === 'text') {
+        return (
+            <Row
+                className={styles.textTargetItem}
+                gutter={16}
+                onMouseEnter={() => setExtraVisible(true)}
+                onMouseLeave={() => setExtraVisible(false)}
+            >
+                <Col span={20} className={styles.content}>
+                    <Tooltip
+                        placement="topLeft"
+                        title={(
+                            <span>
+                                <Avatar shape="circle" size={32} alt="Avatar" src={owner.avatar} style={{ marginRight: '8px' }}/>
+                                <span style={{ lineHeight: '32px' }}>{`${owner.name} at ${moment(owner.updatedAt).format('HH:mm, D MMM')}`}</span>
+                            </span>
+                        )}
+                    >
+                        <Icon type="check-circle" theme="filled" className={styles.icon} />{item.content}
+                    </Tooltip>
+                </Col>
+                <Col span={4} className={styles.extra}>
+                    {item.editable && extraVisible && (
+                        <ButtonGroup>
+                            <Button type="primary" shape="circle" icon="edit"  />
+                            <Button type="primary" shape="circle" icon="rest"  />
+                        </ButtonGroup>
+                    )}
+                    
+                </Col>
+            </Row>
+        );
+    }
+    return (
+        <Row className={styles.inputTargetItem} gutter={16}>
+
+        </Row>
+    )
+};
 
 const Goals = ({ dispatch, match, ...props }) => {
     const [activeKeys, setActiveKeys] = useState(['whatLearn', 'target', 'requirements']);
+    const [whatLearnDisabled, setWhatLearnDisabled] = useState(true);
+    const [requirementsDisabled, setRequirementsDisabled] = useState(true);
+    const [targetStudentsDisabled, setTargetStudentsDisabled] = useState(true);
     const { courseId } = match.params;
     const {
         goals,
         initLoading
     } = props;
+    useEffect(() => {
+        dispatch({
+            type: 'course/fetchGoals',
+            payload: courseId
+        });
+        return () => dispatch({
+            type: 'course/resetGoals'
+        });
+    }, [courseId]);
+    const handleSaveWhatLearn = () => {};
+    const handleSaveRequirements = () => {};
+    const handleSaveTargetStudents = () => {};
     return (
         <div className={styles.goals}>
             <div className={styles.alert}>
                 <Alert
                     message="The descriptions you write here will help students decide if your course is the one for them."
-                    type="info"
+                    type="warning"
                     showIcon
-                    icon={<Icon type="info-circle" theme="filled" style={{ color: '#fe7f9c' }} />}
+                    icon={<Icon type="info-circle" theme="filled" style={{ color: '#fada5e' }} />}
                 />
             </div>
             <div className={styles.main}>
@@ -30,25 +93,54 @@ const Goals = ({ dispatch, match, ...props }) => {
                         activeKey={[]}
                         expandIcon={() => <Icon type="loading" />}
                         expandIconPosition="right"
+                        bordered={false}
                     >
-                        <Panel key="whatLearn" header="What will students learn in your course?" />
-                        <Panel key="requirements" header="Are there any course requirements or prerequisites?" />
-                        <Panel key="target" header="Who are your target students?" />
+                        <Panel key="whatLearn" header="What will students learn in your course?" className={styles.panel}/>
+                        <Panel key="requirements" header="Are there any course requirements or prerequisites?" className={styles.panel}/>
+                        <Panel key="target" header="Who are your target students?" className={styles.panel}/>
                     </Collapse>
                 ) : (
                     <Collapse
                         activeKey={activeKeys}
                         onChange={activeKeys => setActiveKeys(activeKeys)}
                         expandIconPosition="right"
+                        bordered={false}
                     >
                         <Panel key="whatLearn" header="What will students learn in your course?" className={styles.panel}>
-
+                            {_.map(goals.whatLearn, item => (
+                                <TargetItem item={item} key={item._id + _.uniqueId('what_learn_')} onChange={() => setWhatLearnDisabled(false)}/>
+                            ))}
+                            <div className={styles.add}>
+                                <Icon type="plus" />
+                                <span className={styles.text}>Add an answer</span>
+                            </div>
+                            <div className={styles.save}>
+                                <Button type="primary" disabled={whatLearnDisabled} onClick={handleSaveWhatLearn}>Save</Button>
+                            </div>
                         </Panel>
                         <Panel key="requirements" header="Are there any course requirements or prerequisites?" className={styles.panel}>
-
+                            {_.map(goals.requirements, item => (
+                                <TargetItem item={item} key={item._id + _.uniqueId('requirement_')} onChange={() => setRequirementsDisabled(false)}/>
+                            ))}
+                            <div className={styles.add}>
+                                <Icon type="plus" />
+                                <span className={styles.text}>Add an answer</span>
+                            </div>
+                            <div className={styles.save}>
+                                <Button type="primary" disabled={requirementsDisabled} onClick={handleSaveRequirements}>Save</Button>
+                            </div>
                         </Panel>
                         <Panel key="target" header="Who are your target students?" className={styles.panel}>
-
+                            {_.map(goals.targetStudents, item => (
+                                <TargetItem item={item} key={item._id + _.uniqueId('target_student_')} onChange={() => setTargetStudentsDisabled(false)}/>
+                            ))}
+                            <div className={styles.add}>
+                                <Icon type="plus" />
+                                <span className={styles.text}>Add an answer</span>
+                            </div>
+                            <div className={styles.save}>
+                                <Button type="primary" disabled={targetStudentsDisabled} onClick={handleSaveTargetStudents}>Save</Button>
+                            </div>
                         </Panel>
                     </Collapse>
                 )}
