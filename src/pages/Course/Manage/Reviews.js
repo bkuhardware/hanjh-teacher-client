c1dcac0e0b7e9c1ba100a8b73302800c0a8aafcc
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-03-07 21:45:33 +0700

    reviews part 1

--- src/pages/Course/Manage/Reviews.js
+++ src/pages/Course/Manage/Reviews.js
@@ -1,3 +1,59 @@
-export default () => (
-    <div>Reviews</div>
-)
+import React, { useEffect } from 'react';
+import _ from 'lodash';
+import { connect } from 'dva';
+import { Rate, Divider, Icon, Skeleton, Spin } from 'antd';
+import UserAvatar from '@/components/Avatar';
+import styles from './Reviews.less';
+
+const LoadingReview = () => {
+    return (
+        <div className={styles.loadingReview}>
+            <div className={styles.info}>
+                <Skeleton avatar={{ shape: 'circle', size: 60 }} active title={null} paragraph={{ rows: 2, width: ['20%', '15%'] }} />
+            </div>
+            <div className={styles.text}>
+                <Skeleton active title={null} paragraph={{ rows: 2, width: ['100%', '80%'] }} />
+            </div>
+        </div>
+    )
+};
+
+const Reviews = ({ dispatch, match, ...props }) => {
+    const { courseId } = match.params;
+    const {
+        initLoading,
+        loading,
+        reviews
+    } = props;
+    useEffect(() => {
+        dispatch({
+            type: 'manage/fetchReviews',
+            payload: courseId
+        });
+        return () => dispatch({ type: 'manage/resetReviews' });
+    }, [courseId]);
+    return (
+        <div className={styles.reviews}>
+            {!reviews || initLoading ? (
+                <div className={styles.loading}>
+                    <LoadingReview />
+                    <Divider className={styles.divider} dashed/>
+                    <LoadingReview />
+                    <div className={styles.spin}>
+                        <Spin indicator={<Icon spin type="loading" style={{ color: '#fada5e', fontSize: '44px' }}/>} />
+                    </div>
+                </div>
+            ) : (
+                <div />
+            )}
+        </div>
+    )
+};
+
+export default connect(
+    ({ manage, loading }) => ({
+        initLoading: !!loading.effects['manage/fetchReviews'],
+        loading: !!loading.effects['manage/moreReviews'],
+        reviews: manage.reviews
+    })
+)(Reviews);
